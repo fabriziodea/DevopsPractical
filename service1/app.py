83a342e18662d759d1dfe74cef66c4513af3b289
frizzolo <frizzolo@gmail.com>
2021-11-21 13:36:58 +0000

    DPP-10 #DONE Dockerfile added

--- service1/app.py
+++ service1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import requests
 
 app = Flask(__name__)
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def race():
     racenames= requests.get("http:/")
 
-    return renderstuff racepage.html
+    return render_template("racepage.html", records=racenames)
 
-@app.route("/results<bet>")
-def results():
+@app.route("/results/<int:bet>", methods=["GET", "POST"])
+def results(bet):
     winner=int(request.get("http://service3:5002/winner"))
     if winner == bet 
         render template youwon.html
