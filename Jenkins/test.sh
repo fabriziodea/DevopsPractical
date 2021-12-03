74719338588eda31393c0c76f90f18d17682d6db
frizzolo <frizzolo@gmail.com>
2021-12-03 10:43:44 +0000

    DPP-26 # DONE tests completed

---
+++ Jenkins/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt-get install -y python3-pip python3-venv
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r test-requirements.txt
+
+# Test Service1
+cd service1
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+cd ..
+
+# Test Service2
+cd service2
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+cd ..
+
+# Test Service3
+cd service3
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+cd ..
+
+# Test Service4
+cd service4
+python3 -m pytest --cov=application --cov-report xml --cov-report term-missing --junitxml junit.xml
+cd ..
+
+deactivate
